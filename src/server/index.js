ae164b869ac1f1de5605f8bc8f9b8d0e7632dd70
KavyaDevadiga <kdevadiga74@gmail.com>
2024-07-29 11:59:25 +0530

    fixes: log changes for mongo connection

--- src/server/index.js
+++ src/server/index.js
@@ -19,7 +19,7 @@ mongoose.connect("http://localhost:27017", {
 
 const connection = mongoose.connection;
 connection.once("open", () => {
-  console.log("MongoDB database connection established successfully");
+  console.log("Mongo connection established successfully");
 });
 
 const itemRouter = require("./routes/items");
